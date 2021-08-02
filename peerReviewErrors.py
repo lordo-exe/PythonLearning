37863cb924f192dcd2e00efe9a0376c8d4316684
Lordo <kerilivanchuk@gmail.com>
2021-08-02 10:26:40 -0500

    Fixed def chooseCave code errors

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -9,46 +9,44 @@ import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
-	print()
+    print('''You are in a land full of dragons. In front of you,
+    you see two caves. In one cave, the dragon is friendly
+    and will share his treasure with you. The other dragon
+    is greedy and hungry, and will eat you on sight.''')
 
 def chooseCave():
-    cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
-
-	return caves
+    cave = 1
+    while cave != 1 and cave != 2:
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
+        return cave
 
 def checkCave(chosenCave):
-	print('You approach the cave...')
-	#sleep for 2 seconds
-	time.sleep(2)
-	print('It is dark and spooky...')
-	#sleep for 2 seconds
-	time.sleep(3)
-	print('A large dragon jumps out in front of you! He opens his jaws and...')
-	print()
-	#sleep for 2 seconds
-	time.sleep(2)
-	friendlyCave = random.randint(1, 2)
+    print('You approach the cave...')
+    #sleep for 2 seconds
+    time.sleep(2)
+    print('It is dark and spooky...')
+    #sleep for 2 seconds
+    time.sleep(3)
+    print('A large dragon jumps out in front of you! He opens his jaws and...')
+    print()
+    #sleep for 2 seconds
+    time.sleep(2)
+    friendlyCave = random.randint(1, 2)
 
-	if chosenCave == str(friendlyCave):
-		print('Gives you his treasure!')
-	else:
-		print 'Gobbles you down in one bite!'
+    if chosenCave == str(friendlyCave):
+        print('Gives you his treasure!')
+    else:
+        print 'Gobbles you down in one bite!'
 
 playAgain = 'yes'
 while playAgain = 'yes' or playAgain = 'y':
-	displayIntro()
-	caveNumber = choosecave()
-	checkCave(caveNumber)
+    displayIntro()
+    caveNumber = choosecave()
+    checkCave(caveNumber)
     
-	print('Do you want to play again? (yes or no)')
-	playAgain = input()
-	if playAgain == "no":
-		print("Thanks for planing")
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
+    if playAgain == "no":
+        print("Thanks for planing")
 
