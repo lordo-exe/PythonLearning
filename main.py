ca64329217287ac859b09d144a39632295157efc
Lordo <kerilivanchuk@gmail.com>
2021-08-11 15:17:14 -0500

    Created dir Assignment9.1 Created code in main, line 1, 9-15

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+
 print("Hello World")
 
 # hello world print record
 
 print("Welcome to my first GIT repo!")
+
+user = input("Please input directory name to verify: ")
+
+isFile = os.path.isdir(user)
+print("Does this file exist?", isFile)
+
+if not isFile:
+    print("This directory does not exist, please retry.")
+
