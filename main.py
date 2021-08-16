f4509413ec067ea3ce65197957ea270a6836bbf8
Lordo <kerilivanchuk@gmail.com>
2021-08-16 07:29:12 -0500

    Created dir Assignment9n1

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ print("Welcome to my first GIT repo!")
 user = input("Please input directory name to verify: ")
 
 isFile = os.path.isdir(user)
-print("Does this file exist?", isFile)
+print("Does this directory exist?", isFile)
 
 if not isFile:
     print("This directory does not exist, please retry.")
+
+if isFile:
+   ## os.chdir(C:\Users\Lord\Desktop\gitRepo\Assignment9n1)
+    print(os.getcwd())
+
+
 
