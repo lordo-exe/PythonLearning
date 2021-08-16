6235a403c4c2aa71361f9724cd323d3a9e1643ea
Lordo <kerilivanchuk@gmail.com>
2021-08-16 07:53:58 -0500

    Created a program allowing you to choose a dir and create a custom text file within the chosen dir. You are prompted to add name, address, and phone number within this program.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ print("Hello World")
 
 # hello world print record
 
-print("Welcome to my first GIT repo!")
+print("Welcome to my first GIT repo!\n")
 
-user = input("Please input directory name to verify: ")
+user = input("In order to create a file please choose a directory by name: ")
 
 isFile = os.path.isdir(user)
 print("Does this directory exist?", isFile)
@@ -15,8 +15,21 @@ if not isFile:
     print("This directory does not exist, please retry.")
 
 if isFile:
-   ## os.chdir(C:\Users\Lord\Desktop\gitRepo\Assignment9n1)
+    os.chdir(user)
     print(os.getcwd())
 
+filename = input("Please name your text file: ")
+nameExample = input("Please input your first and last name: ")
+addressExample = input("Please input your full address: ")
+phoneExample = input("Please input your full phone number: ")
 
+f = open(f'{filename}.txt', 'x')
+f.write(f"Name: {nameExample} \nAddress: {addressExample} \nPhone Number: {phoneExample}")
+f.close()
 
+print(f"Here are the contents of your new file named {filename}, in directory: {user}.")
+
+f = open('exampleFile.txt', 'r')
+print(f.read())
+
+print("\nEnd of program.")
